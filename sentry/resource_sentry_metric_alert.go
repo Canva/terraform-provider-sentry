07a988a523f97e255bef672ebff8316753133b00
Taj Pereira <taj@canva.com>
2022-09-27 09:44:04 +0930

    Updated metric alert to include integration id for slack action (#39)
    * feat: add integration ID to metric alert
    
    * docs: add more docs
    
    * Update docs/index.md
    
    * Update docs/resources/metric_alert.md
    
    * Update sentry/resource_sentry_metric_alert.go
    
    * Update sentry/resource_sentry_metric_alert_test.go
    
    * deps: use 1.4.2 of go-sentry

--- sentry/resource_sentry_metric_alert.go
+++ sentry/resource_sentry_metric_alert.go
@@ -118,6 +118,10 @@ func resourceSentryMetricAlert() *schema.Resource {
 										Type:     schema.TypeString,
 										Required: true,
 									},
+									"integration_id": {
+										Type:     schema.TypeInt,
+										Optional: true,
+									},
 								},
 							},
 						},
@@ -340,6 +344,9 @@ func expandMetricAlertTriggerActions(actionList []interface{}) []*sentry.MetricA
 				action.ID = sentry.String(v)
 			}
 		}
+		if v, ok := actionMap["integration_id"].(int); ok && v != 0 {
+			action.IntegrationID = sentry.Int(v)
+		}
 		actions = append(actions, action)
 	}
 	return actions
@@ -376,8 +383,11 @@ func flattenMetricAlertTriggerActions(actions []*sentry.MetricAlertTriggerAction
 		actionMap["type"] = action.Type
 		actionMap["target_type"] = action.TargetType
 		actionMap["target_identifier"] = action.TargetIdentifier
+		actionMap["integration_id"] = action.IntegrationID
+
 		actionList = append(actionList, actionMap)
 	}
+
 	return actionList
 }
 
